818be503bd8117b31cd3a0b17658723210b96b2c
Gabriel de Souza Cruz <12989644660@campus.machado.mch>
2025-04-24 10:28:58 -0300

    first

---
+++ controllers/FilaController.js
@@ -0,0 +1,36 @@
+const minhaFila = new Fila(5);
+
+function addElemento(){
+    const novoElemento = 
+       document.getElementById("txtnovoNome");
+    if(!minhaFila.isFull()){
+       minhaFila.enqueue(novoElemento.value);
+       mostrarFila();
+       novoElemento.value = ""; //limpar
+       novoElemento.focus(); //cursor no input
+    } 
+    else
+        alert("Fila cheia!");     
+}//fim addElemento
+//------------------------------------------------------------------------------------\\
+
+function mostrarFila(){
+   const listaFila = document.getElementById("listFila");
+   listaFila.textContent = minhaFila.toString();
+}//fim mostrarFila
+//-------------------------------------------------------------------------------------\\
+
+function atenderFila(){
+   if(!minhaFila.isEmpty()){
+      const atendido = minhaFila.dequeue();
+      //alert("Pessoa atendida: " + atendido); abre um alerta
+      //salvar no banco texto do navegador
+      const alerta = document.getElementById("mensagem-remocao");
+      alerta.textContent = ("Pessoa atendida: " + atendido); //imprime diretamente na aba (elemento:mensagem-remocao)
+      mostrarFila();
+   }
+   else
+      alert("Fila vazia!");
+
+   
+}
